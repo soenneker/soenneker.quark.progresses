581aab63e0e36c4c9187772db4187a11484aec56
Jake Soenneker <jake@soenneker.com>
2025-09-14 11:41:38 -0500

    Initial commit

---
+++ test/Soenneker.Quark.Progresses.Tests/ProgressTests.cs
@@ -0,0 +1,18 @@
+﻿using Soenneker.Tests.FixturedUnit;
+using Xunit;
+
+namespace Soenneker.Quark.Progresses.Tests;
+
+[Collection("Collection")]
+public sealed class ProgressTests : FixturedUnitTest
+{
+    public ProgressTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    {
+    }
+
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
